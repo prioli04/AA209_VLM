495bef89841abcf10640772b2e345dd00a9571da
EPRIOLI <enricoprioli57@gmail.com>
2025-11-12 11:36:36 -0300

    Add cosine, sine and negative sine mesh discretization

--- VLM/WingPatch.py
+++ VLM/WingPatch.py
@@ -1,13 +1,37 @@
 from .Airfoil import Airfoil
 from .Section import Section
+from enum import Enum, auto
 
 import numpy as np
 
+class DiscretizationType(Enum):
+        UNIFORM = auto()
+        COSINE = auto()
+        SINE = auto()
+        MINUS_SINE = auto()
+
 class WingPatch:
-    def __init__(self, nx: int, ny: int):
+    def __init__(self, nx: int, ny: int, disc_type_x: DiscretizationType, disc_type_y: DiscretizationType):
         self.nx, self.ny = nx, ny
+        self._disc_type_x, self._disc_type_y = disc_type_x, disc_type_y
         self._section_left: Section | None = None
         self._section_right: Section | None = None
+
+    def _discretize_vals(self, x_min: float, x_max: float, n: int, disc_type: DiscretizationType):
+        if disc_type == DiscretizationType.UNIFORM:
+            return np.linspace(x_min, x_max, n)
+        
+        elif disc_type == DiscretizationType.COSINE:
+            x = np.linspace(0.0, np.pi, n)
+            return ((x_max - x_min) / 2.0) * (np.cos(x + np.pi) + 1.0) + x_min
+
+        elif disc_type == DiscretizationType.SINE:
+            x = np.linspace(0.0, np.pi / 2.0, n)
+            return (x_max - x_min) * (np.sin(x - np.pi / 2.0) + 1.0) + x_min
+        
+        elif disc_type == DiscretizationType.MINUS_SINE:
+            x = np.linspace(0.0, np.pi / 2.0, n)
+            return -(x_max - x_min) * np.sin(x + np.pi) + x_min
 
     def _apply_sections(self, root: Section, tip: Section, corners_x: np.ndarray, corners_y: np.ndarray, corners_z: np.ndarray, wing_root_chord: float, wing_semi_span: float):
         airfoil_root = Airfoil.read(root.airfoil_path)
@@ -37,8 +61,8 @@ class WingPatch:
         if root is None or tip is None:
             raise ValueError("Sections not initialized.")
 
-        x = np.linspace(0.0, 1.0, self.nx + 1)
-        y = wing_semi_span * np.linspace(root.fy_pos, tip.fy_pos, self.ny + 1)
+        x = self._discretize_vals(0.0, 1.0, self.nx + 1, self._disc_type_x)
+        y = wing_semi_span * self._discretize_vals(root.fy_pos, tip.fy_pos, self.ny + 1, self._disc_type_y)
 
         corners_x, corners_y = np.meshgrid(x, y, indexing="ij")
         corners_z = np.zeros_like(corners_x)
--- VLM_Run.py
+++ VLM_Run.py
@@ -4,19 +4,20 @@
 
 import cProfile
 import VLM
+from VLM.WingPatch import DiscretizationType
 
 sections = [
     VLM.Section(fy_pos=0.0, fc=1.0, x_offset=0.0, twist_deg=0.0, airfoil_path_str="S1223.dat"),
-    VLM.Section(fy_pos=0.5, fc=1.0 ,x_offset=0.0, twist_deg=-5.0, airfoil_path_str="naca0012.dat"),
-    VLM.Section(fy_pos=1.0, fc=1.0, x_offset=0.0, twist_deg=-10.0, airfoil_path_str="naca0012.dat")
+    VLM.Section(fy_pos=0.5, fc=0.75 ,x_offset=0.03, twist_deg=-5.0, airfoil_path_str="naca0012.dat"),
+    VLM.Section(fy_pos=1.0, fc=0.5, x_offset=0.1, twist_deg=-10.0, airfoil_path_str="naca0012.dat")
 ]
 
 patches = [
-    VLM.WingPatch(20, 15),
-    VLM.WingPatch(20, 15)
+    VLM.WingPatch(20, 5, DiscretizationType.COSINE, DiscretizationType.UNIFORM),
+    VLM.WingPatch(20, 8, DiscretizationType.COSINE, DiscretizationType.MINUS_SINE)
 ]
 
-wing_geom = VLM.WingGeometry(sections, patches, b=2.5, AR=3.876)
+wing_geom = VLM.WingGeometry(sections, patches, b=2.5, AR=5.0)
 
 params = VLM.Parameters(
     V_inf = 12.0, 
@@ -26,11 +27,11 @@ params = VLM.Parameters(
     b = wing_geom.b, 
 
     n_wake_deform = 0, 
-    wake_dt_fact = 1e6, 
+    wake_dt_fact = 0, 
     wake_dx_fact = 0.3,
 
-    CL_tol = 1e-4,
-    CD_tol = 1e-5,
+    CL_tol = 1e-0,
+    CD_tol = 1e-0,
 
     sym=True,
     ground=False
