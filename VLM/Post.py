Fix trefftz plane wake normal calculation

VLM/Post.py
@@ -90,9 +90,9 @@ class Post:
         M = np.sum(delta_M, axis=0)
         M_stab = T_stab @ M
 
-        CMl = -M_stab[0] / (self._q_inf * S_ref * self._b)
+        CMl = -M_stab[0] / (self._q_inf * S_ref * self._b) if not self._sym else 0.0
         CM = M_stab[1] / (self._q_inf * S_ref * self._MAC)
-        CN = -M_stab[2] / (self._q_inf * S_ref * self._b)
+        CN = -M_stab[2] / (self._q_inf * S_ref * self._b) if not self._sym else 0.0
 
         delta_phi_i = Gammas[-1, :]
         delta_L = self._rho * self._V_inf * delta_phi_i * np.cos(theta_i) * delta_s_i
@@ -103,7 +103,7 @@ class Post:
         Cd_sec = delta_Di / (self._q_inf * self._delta_y * self._chords)
 
         CL = np.sum(delta_L) / (self._q_inf * S_ref)
-        CY = np.sum(delta_Y) / (self._q_inf * S_ref)
+        CY = np.sum(delta_Y) / (self._q_inf * S_ref) if not self._sym else 0.0
         CD = np.sum(delta_Di) / (self._q_inf * S_ref)
 
         coefs_3D = Result.Coefs_3D(CL, CD, CY, CMl, CM, CN)

VLM/Solver.py
@@ -94,7 +94,7 @@ class Solver:
                 P1, P2 = C14[j, :], C14[j + 1, :]
 
                 V_ind_trefftz = Solver.bij_trefftz(CP, P1, P2, 1.0, self._params.sym, self._params.ground)
-                B_trefftz[i, j] = np.dot(V_ind_trefftz, normal[1:])
+                B_trefftz[i, j] = np.dot(V_ind_trefftz, normal)
 
         return B_trefftz
 

VLM/WingPanels.py
@@ -133,10 +133,11 @@ class WingPanels(PanelGrid):
         return normals
     
     def normal_TREFFTZ(self):
-        normalX = self._normalX[-1, :].reshape(-1, 1)
         normalY = self._normalY[-1, :].reshape(-1, 1)
         normalZ = self._normalZ[-1, :].reshape(-1, 1)
-        return np.hstack((normalX, normalY, normalZ))
+        normals = np.hstack((normalY, normalZ))
+
+        return normals / np.linalg.norm(normals, axis=1).reshape(-1, 1)
 
     def plot_mesh(self, ax: Axes3D):
         if self._sym: